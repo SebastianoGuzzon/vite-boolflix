73ff21e675a369bfa80441a9044ece771c97f068
Sebastiano <sebyguzz@icloud.com>
2023-05-08 10:29:26 +0200

    Modifiche commenti e js

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -17,12 +17,12 @@ export default {
           class="logo">BOOLFLIX</div>
 
         <!-- **Milestone 1:**
-Creare un layout base con una searchbar (una input e un button) in cui possiamo scrivere completamente o parzialmente il nome di un film. Possiamo, cliccando il  bottone, cercare sull’API tutti i film che contengono ciò che ha scritto l’utente.
-Vogliamo dopo la risposta dell’API visualizzare a schermo i seguenti valori per ogni film trovato: 
-Titolo
-Titolo Originale
-Lingua
-Voto -->
+        Creare un layout base con una searchbar (una input e un button) in cui possiamo scrivere completamente o parzialmente il nome di un film. Possiamo, cliccando il  bottone, cercare sull’API tutti i film che contengono ciò che ha scritto l’utente.
+        Vogliamo dopo la risposta dell’API visualizzare a schermo i seguenti valori per ogni film trovato: 
+        Titolo
+        Titolo Originale
+        Lingua
+        Voto -->
 
         <div class="title">
           <h1>Film, serie TV e tanto altro.</h1>
--- src/store.js
+++ src/store.js
@@ -1,6 +1,12 @@
 import { reactive } from 'vue'
 import axios from 'axios'
 
+//   **Milestone 3:**
+// In questa milestone come prima cosa aggiungiamo la copertina del film o della serie al nostro elenco. 
+// Trasformiamo poi il voto da 1 a 10 decimale in un numero intero da 1 a 5, così da permetterci di stampare a schermo un numero di stelle piene che vanno da 1 a 5, lasciando le restanti vuote (troviamo le icone in FontAwesome).
+// Arrotondiamo sempre per eccesso all’unità successiva, non gestiamo icone mezze piene (o mezze vuote)
+
+
 export const store = reactive({
 
   unloackButton: true,
@@ -10,6 +16,8 @@ export const store = reactive({
   searchTitle: '',
 
   callApi(name) {
+
+    // Ci viene passata dall’API solo la parte finale dell’URL, questo perché poi potremo generare da quella porzione di URL tante dimensioni diverse. Dovremo prendere quindi l’URL base delle immagini di TMDB: https://image.tmdb.org/t/p/ per poi aggiungere la dimensione che vogliamo generare (troviamo tutte le dimensioni possibili a questo link: https://www.themoviedb.org/talk/53c11d4ec3a3684cf4006400) per poi aggiungere la parte finale dell’URL passata dall’API.
 
     this.media = null;
     this.callAxios('https://api.themoviedb.org/3/search/movie?api_key=ab909735a57a0d14313842405a2fd07c&query=' + name)
